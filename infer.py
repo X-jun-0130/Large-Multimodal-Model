b2b944e590231cc6f6c637882b019b888f486344
No_GPT <46883256+X-jun-0130@users.noreply.github.com>
2023-12-01 14:48:20 +0800

    模型推理

--- infer.py
+++ infer.py
@@ -11,9 +11,9 @@ import warnings
 warnings.filterwarnings("ignore")
 
 para_dict = {
-            "lm_model_name_or_path":"/workspace/Xuxiangjun/Model_WiNGPT_FT/WiNGPT-002-0819/",
+            "lm_model_name_or_path":"/Model_pretrain/Chinese_llama2_7B/epoch3/",
             "vision_model_name_or_path":"/data/public/clip-vit-large-patch14/",
-            "checkpoint_path":"/workspace/Xuxiangjun/Model_WiNGPT_FT/multi-modal-llama7B/",
+            "checkpoint_path":"/Model_FT/multi-modal-llama7B/",
             "num_gpus":1,
             "device":'cuda',
             "max_gpu_memory":None,
@@ -86,11 +86,7 @@ def get_model_input(data_format):
 
     return image_tensor, input_ids
 
-ttt = {"text": "User: 单选题：引起疼痛的炎症介质是()\nA.活性氧\nB.补体\nC.溶酶体酶\nD.淋巴因子\nE.前列腺素E2</s>\n Assistant: 【解析】由表4-2可知，引起疼痛的炎症介质主要有前列腺素、缓激肽、P物质(E对)。\n答案:E</s>\n User: <im_start><image><im_end>\n做一个ocr任务，输出照片中的内容。</s>\n Assistant: 什么</s>\n User: <im_start><image><im_end>\n进行一个OCR任务，帮忙将脑脊液检查项目的内容以表格的形式输出。</s>\n Assistant: 脑脊液检查\n```|项目名称|结果|参考范围|单位|\n|:-----------|:-------------|:-------------|:--------|\n|细菌|有|无||\n|精定性|阴性|弱阳性||\n|透明度|清洗透明|清洗透明||\n|颜色|无色水样液体|无色水样液体||\n|蛋白质定性|阳性|阴性||\n|细胞计数|0.2|0-8|10^16/L|```</s>\n User: 你能找出这个序列中缺失的数字吗：1，2，_，4，7？提示：缺失的数字在2和4之间。</s>\n Assistant: 3。</s>\n User: What makes this cooking scenario unique,and what challenges might the cooks face?\n<im_start><image><im_end></s>\n Assistant: The unique aspect of this cooking scenario is that several people are preparing a meal on the back of a moving boat at sea.This presents several challenges that the cooks might face,which are different from cooking in conventional indoor settings.First,they have to deal with the impact of the boat's movement on the cooking process,such as the potential for spills or accidents.Secondly,they may have limited space and resources available on the boat,which could affect the type and complexity of the meals they can prepare.Additionally,the cooks must consider the weather conditions and waves while cooking,as these factors can also affect stability and safety during food preparation.Overall,cooking on a boat requires a higher level of adaptability and coordination between the cooks to ensure that the meal is prepared safely and efficiently.</s>\n User: 如何以Swift创建一个社交媒体广告活动，有效地展示产品X的创新硬件突破？该广告活动应包括文本、图片和视频的混合，以吸引潜在客户并传达其独特的优势。您能提供一些Swift代码示例，以将这些不同的元素整合成一个有影响力的广告活动吗？</s>\n Assistant: 很抱歉，作为AI语言模型，我无法提供Swift代码示例。但是，我可以提供一些建议和指导，帮助您创建一个有影响力的社交媒体广告活动。\n\n1. 确定目标受众和平台：在创建广告活动之前，您需要确定您的目标受众和他们使用的社交媒体平台。例如，如果您的目标受众是年轻人，您可能需要在Instagram或TikTok上发布广告。如果您的目标受众是专业人士，您可能需要在LinkedIn上发布广告。\n\n2. 创意设计：您需要设计一个有吸引力的广告，以吸引潜在客户并传达产品X的独特优势。您可以使用文本、图片和视频的混合来创造一个有影响力的广告。确保您的广告设计与您的品牌形象一致，并使用清晰、简洁的语言来传达您的信息。\n\n3. 广告投放：一旦您的广告设计完成，您需要选择一个合适的广告投放平台，并设置广告投放参数，例如广告预算、广告投放时间和广告投放地点。您可以使用社交媒体广告平台，例如Facebook Ads或Google Ads，来投放广告。\n\n4. 广告效果跟踪：最后，您需要跟踪广告效果，并根据数据进行优化。您可以使用广告平台提供的分析工具来跟踪广告效果，并根据数据进行调整，以提高广告的效果和转化率。\n\n总之，创建一个有影响力的社交媒体广告活动需要一定的策略和创意设计。通过确定目标受众、创意设计、广告投放和广告效果跟踪，您可以创建一个成功的广告活动，有效地展示产品X的创新硬件突破。</s>\n User: <im_start><image><im_end>\n能否做一个文字识别的工作，给出所提供图的文字信息。</s>\n Assistant: 以防止食物转化为</s>",
-  "image_list": ["/workspace/Xuxiangjun/Images/chinese_text_recognition/248786.png", "/workspace/Xuxiangjun/Images/Medical-Checkup-Report-OCR/20220623110401-2998.png", "/workspace/Xuxiangjun/Images/coco-2017/000000452470.jpg", "/workspace/Xuxiangjun/Images/chinese_text_recognition/193610.png"]}
-
-data_format = [{'image_path':ttt['image_list'], 'user':ttt['text'], 'assistant':''},
-               #{'image_path':[], 'user':'边缘区淋巴瘤的分期系统一般采用哪种？', 'assistant':''}
+data_format = [{'image_path':['https://img2.baidu.com/it/u=2705133129,1120911323&fm=253&fmt=auto&app=138&f=JPEG?w=609&h=500'], 'user':'描述一下这副美丽的图片.', 'assistant':''},
                ]
 
 image_tensor, input_ids = get_model_input(data_format)
@@ -102,7 +98,7 @@ model,  _  = create_dsvl_model_and_transforms(
 
 model.load_state_dict(torch.load(os.path.join(para_dict['checkpoint_path'], 'pytorch_hf.bin'), map_location='cpu'), strict=False) 
 
-model = model.to('cpu')
+model = model.to('cuda')
 model = model.eval()
 
 # outputs = model.lang_decoder.generate(input_ids, max_new_tokens=56)
